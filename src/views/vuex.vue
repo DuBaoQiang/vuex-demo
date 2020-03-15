92b48149410cb89c6a4bbb44ba7c43d09f266c2e
Du_baoqiang <Du_baoqiang@126.com>
2020-03-15 16:53:43 +0800

    add watch

--- src/views/vuex.vue
+++ src/views/vuex.vue
@@ -1,22 +1,27 @@
 <template>
     <div class="index">
-        <h1>{{user.name}}</h1>
-        <h1>{{user.age}}</h1>
+        <h1>{{userInfo.name}}</h1>
+        <h1>{{userInfo.age}}</h1>
         <el-button @click="changeData">点击按钮</el-button>
     </div>
 </template>
 <script>
+    import {mapState} from 'vuex'
     export default {
         name: 'Index',
         data() {
             return{
-                msg: '12323'
+                msg: '12323',
+                obj: {}
             }
         },
         computed:{
             user() {
                 return this.$store.state.info
-            }
+            },
+            ...mapState({
+                userInfo: state => state.info
+            })
         },
         methods:{
             changeData() {
@@ -25,6 +30,15 @@
                     age: 27
                 }
                 this.$store.commit('changeUserInfo', params)
+            }
+        },
+        watch: {
+            obj: {
+                deep: true,
+                immediate: true,
+                handler(newValue, oldValue) {
+
+                }
             }
         }
     }
